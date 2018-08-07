4db2850b108c462a5d71818fce9c6451067527b5
chenhaoxiang <chenhaoxiang0117@qq.com>
2018-08-07 21:44:21 +0800

    使用XML静态映射

--- ssm-spring-mvc/src/main/java/com/uifuture/basics/config/MvcConfig.java
+++ ssm-spring-mvc/src/main/java/com/uifuture/basics/config/MvcConfig.java
@@ -4,44 +4,39 @@
  */
 package com.uifuture.basics.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
-
 /**
  * @author chenhx
  * @version MvcConfig.java, v 0.1 2018-08-04 下午 7:05
  */
-@Configuration
-@EnableWebMvc
-@ComponentScan("com.uifuture.basics")
-public class MvcConfig implements WebMvcConfigurer {
-    @Bean
-    public InternalResourceViewResolver viewResolver() {
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        /**
-         * 映射路径-运行时代码会将我们的页面自动编译到/WEB-INF/classes/views/下
-         */
-        viewResolver.setPrefix("/WEB-INF/classes/views/");
-        /**
-         * 实际页面后缀
-         */
-        viewResolver.setSuffix(".jsp");
-        viewResolver.setViewClass(JstlView.class);
-        return viewResolver;
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        //配置url中的路径
-        registry.addResourceHandler("/static/**")
-                //配置静态文件的路径
-                .addResourceLocations("classpath:/static/");
-    }
-
-}
+//@Configuration
+//@EnableWebMvc
+//@ComponentScan("com.uifuture.basics")
+//public class MvcConfig implements WebMvcConfigurer {
+//    @Bean
+//    public InternalResourceViewResolver viewResolver() {
+//        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+//        /**
+//         * 映射路径-运行时代码会将我们的页面自动编译到/WEB-INF/classes/views/下
+//         */
+//        viewResolver.setPrefix("/WEB-INF/classes/views/");
+//        /**
+//         * 实际页面后缀
+//         */
+//        viewResolver.setSuffix(".jsp");
+//        viewResolver.setViewClass(JstlView.class);
+//        return viewResolver;
+//    }
+//
+//    /**
+//     * 使用编程式代码配置静态页面的映射
+//     * @param registry
+//     */
+////    @Override
+////    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+////        //配置url中的路径
+////        registry.addResourceHandler("/static/**")
+////                //配置静态文件的路径
+////                .addResourceLocations("classpath:/static/");
+////    }
+//
+//}
--- ssm-spring-mvc/src/main/java/com/uifuture/basics/config/WebInitializer.java
+++ ssm-spring-mvc/src/main/java/com/uifuture/basics/config/WebInitializer.java
@@ -5,11 +5,10 @@
 package com.uifuture.basics.config;
 
 import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.context.support.XmlWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 /**
@@ -21,19 +20,21 @@ import javax.servlet.ServletRegistration;
 public class WebInitializer implements WebApplicationInitializer {
 
     @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
-        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
-        context.register(MvcConfig.class);
-        /**
-         * 新建WebApplicationContext,注册配置类，并将其和当前servletContext关联
-         */
-        context.setServletContext(servletContext);
+    public void onStartup(ServletContext servletContext) {
+        //一、与xml文件组合使用
+        XmlWebApplicationContext appContext = new XmlWebApplicationContext();
+        appContext.setConfigLocation("/WEB-INF/classes/spring-mvc-servlet.xml");
+
+        //二、使用编程式方式代码配置
+//        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+//        appContext.register(MvcConfig.class);
+        //新建WebApplicationContext,注册配置类，并将其和当前servletContext关联
+//        appContext.setServletContext(servletContext);
 
         //注册并映射调度程序servlet
-        ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(context));
-        /**
-         * 注册Spring MVC的DispatcherServlet
-         */
+        ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher",
+                new DispatcherServlet(appContext));
+        //注册Spring MVC的DispatcherServlet，替代web.xml的使用
         servlet.addMapping("/");
         servlet.setLoadOnStartup(1);
     }
