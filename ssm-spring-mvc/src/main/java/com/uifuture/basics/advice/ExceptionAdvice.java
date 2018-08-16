43cc28f38cec970bb5f1a42486dab7268d96d8dd
chenhaoxiang <chenhaoxiang0117@qq.com>
2018-08-16 20:10:38 +0800

    演示通过ViewResolver进行请求转发

--- ssm-spring-mvc/src/main/java/com/uifuture/basics/advice/ExceptionAdvice.java
+++ ssm-spring-mvc/src/main/java/com/uifuture/basics/advice/ExceptionAdvice.java
@@ -4,6 +4,8 @@
  */
 package com.uifuture.basics.advice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -24,6 +26,7 @@ import java.util.Map;
 @ControllerAdvice
 public class ExceptionAdvice {
 
+    private Logger logger = LoggerFactory.getLogger(ExceptionAdvice.class);
     /**
      * 控制所有控制器方法的Model返回值
      *
@@ -31,18 +34,18 @@ public class ExceptionAdvice {
      */
     @ModelAttribute
     public void modelAttribute(Model model) {
-        System.out.println("把返回值放入Model");
+        logger.info("把返回值放入Model");
         model.addAttribute("globalName", "SpringMVC");
     }
 
     /**
      * 进行所有控制器的初始化数据转换，绑定
-     *
+     *  在传递参数的时候就好先执行这个方法
      * @param binder
      */
     @InitBinder
     public void initBinder(WebDataBinder binder) {
-        System.out.println("执行初始化数据绑定器");
+        logger.info("执行初始化数据绑定器");
     }
 
     /**
