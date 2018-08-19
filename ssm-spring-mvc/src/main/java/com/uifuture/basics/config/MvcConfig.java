2131ad99c9ba70cf75d6ad893b1c589dcf484fe8
chenhaoxiang <chenhaoxiang0117@qq.com>
2018-08-19 18:16:29 +0800

    使用GzipResourceResolver进行静态资源压缩的加载

--- ssm-spring-mvc/src/main/java/com/uifuture/basics/config/MvcConfig.java
+++ ssm-spring-mvc/src/main/java/com/uifuture/basics/config/MvcConfig.java
@@ -7,6 +7,8 @@ package com.uifuture.basics.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -24,6 +26,21 @@ import org.springframework.web.servlet.view.JstlView;
 @EnableWebMvc
 @ComponentScan("com.uifuture.basics")
 public class MvcConfig implements WebMvcConfigurer {
+    /**
+     * 文件上传解析器
+     *
+     * @return
+     */
+    @Bean
+    public MultipartResolver multipartResolver() {
+        CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
+        //文件上传大小限制为50MB
+        commonsMultipartResolver.setMaxUploadSize(1024 * 1024 * 50);
+        //设置编码为utf-8
+        commonsMultipartResolver.setDefaultEncoding("UIF-8");
+        return commonsMultipartResolver;
+    }
+
     /**
      * 注册JSP页面视图解析器
      *
@@ -56,7 +73,7 @@ public class MvcConfig implements WebMvcConfigurer {
                 //配置静态文件的路径,最后需要"/"结尾，否则会访问不到静态资源
                 .addResourceLocations("classpath:/static/")
                 //设置静态资源的缓存时间，单位为S
-                .setCachePeriod(60 * 60 * 24 * 365)
+                .setCachePeriod(60 * 60 *24*365)
                 .resourceChain(true)
                 .addResolver(new GzipResourceResolver())
                 //如果是资源处理链，则将默认的PathResourceResolver路径资源解析器放到最后（Spring推荐）
@@ -69,7 +86,6 @@ public class MvcConfig implements WebMvcConfigurer {
      * 定义ParameterizableViewController的简单方式、
      * 当该控制器被访问的时候没有任何操作，只是进行相应的请求映射，则可以使用该方法
      * 章节:7.2.1
-     *
      * @param registry
      */
     @Override
@@ -78,5 +94,7 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addViewController("/cacheController/index")
                 //设置视图名称 - 对应文件路径
                 .setViewName("/cache/index");
+        //映射文件上传页面
+        registry.addViewController("/file/upload").setViewName("/upload/index");
     }
 }
--- ssm-spring-mvc/src/main/resources/views/cache/index.jsp
+++ ssm-spring-mvc/src/main/resources/views/cache/index.jsp
@@ -5,7 +5,7 @@
 -->
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<html>
+<html> utils
 <head>
     <title>Title</title>
     <link rel="stylesheet"
