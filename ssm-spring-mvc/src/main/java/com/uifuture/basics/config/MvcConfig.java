0695faf98e216cea723d6bf494ca7a9e6c94c274
chenhaoxiang <chenhaoxiang0117@qq.com>
2018-08-19 16:31:03 +0800

    使用GzipResourceResolver进行静态资源压缩的加载

--- ssm-spring-mvc/src/main/java/com/uifuture/basics/config/MvcConfig.java
+++ ssm-spring-mvc/src/main/java/com/uifuture/basics/config/MvcConfig.java
@@ -9,10 +9,14 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.GzipResourceResolver;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 /**
+ * ComponentScan - 启用Controller Bean的自动检测
  * @author chenhx
  * @version MvcConfig.java, v 0.1 2018-08-04 下午 7:05
  */
@@ -49,8 +53,30 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         //配置url中的路径
         registry.addResourceHandler("/static/**")
-                //配置静态文件的路径
-                .addResourceLocations("classpath:/static/");
+                //配置静态文件的路径,最后需要"/"结尾，否则会访问不到静态资源
+                .addResourceLocations("classpath:/static/")
+                //设置静态资源的缓存时间，单位为S
+                .setCachePeriod(60 * 60 * 24 * 365)
+                .resourceChain(true)
+                .addResolver(new GzipResourceResolver())
+                //如果是资源处理链，则将默认的PathResourceResolver路径资源解析器放到最后（Spring推荐）
+                .addResolver(new PathResourceResolver())
+        ;
     }
 
+    /**
+     * 配置直接映射
+     * 定义ParameterizableViewController的简单方式、
+     * 当该控制器被访问的时候没有任何操作，只是进行相应的请求映射，则可以使用该方法
+     * 章节:7.2.1
+     *
+     * @param registry
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        //添加访问路径
+        registry.addViewController("/cacheController/index")
+                //设置视图名称 - 对应文件路径
+                .setViewName("/cache/index");
+    }
 }
