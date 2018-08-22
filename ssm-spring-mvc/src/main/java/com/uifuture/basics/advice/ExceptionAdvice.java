deb0552523b77a066fef72573fec1571d153ab0f
chenhaoxiang <chenhaoxiang0117@qq.com>
2018-08-22 21:50:53 +0800

    屏蔽注解

--- ssm-spring-mvc/src/main/java/com/uifuture/basics/advice/ExceptionAdvice.java
+++ ssm-spring-mvc/src/main/java/com/uifuture/basics/advice/ExceptionAdvice.java
@@ -8,7 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -23,7 +22,7 @@ import java.util.Map;
  * @author chenhx
  * @version ExceptionAdvice.java, v 0.1 2018-08-07 下午 9:47
  */
-@ControllerAdvice
+//@ControllerAdvice
 public class ExceptionAdvice {
 
     private Logger logger = LoggerFactory.getLogger(ExceptionAdvice.class);
