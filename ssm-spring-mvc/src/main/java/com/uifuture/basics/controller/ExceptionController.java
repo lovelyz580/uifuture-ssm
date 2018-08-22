f4e8716bd20ff671397abb57f9be2ba000f96143
chenhaoxiang <chenhaoxiang0117@qq.com>
2018-08-22 21:37:20 +0800

    使用HandlerExceptionResolver接口进行自定义异常处理

---
+++ ssm-spring-mvc/src/main/java/com/uifuture/basics/controller/ExceptionController.java
@@ -0,0 +1,45 @@
+/**
+ * uifuture.com
+ * Copyright (C) 2013-2018 All Rights Reserved.
+ */
+package com.uifuture.basics.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 演示异常的抛出Controller
+ * 章节:8.1
+ *
+ * @author chenhx
+ * @version ExceptionController.java, v 0.1 2018-08-22 下午 9:29
+ */
+@Controller
+@RequestMapping("exception")
+public class ExceptionController {
+
+    private Logger logger = LoggerFactory.getLogger(ExceptionController.class);
+
+    /**
+     * 根据传进来的code不同，抛出不同的运行时异常
+     *
+     * @param code
+     * @return
+     */
+    @RequestMapping("${code}")
+    public String exception(@PathVariable("code") Integer code) {
+        logger.info("code={}", code);
+        if (code.equals(1)) {
+            throw new HttpMessageNotWritableException("code等于1，抛出BindException异常");
+        } else if (code.equals(2)) {
+            throw new HttpMessageNotReadableException("code等于2，抛出HttpMessageNotReadableException异常");
+        }
+        throw new RuntimeException("其他的运行时异常");
+    }
+
+}
