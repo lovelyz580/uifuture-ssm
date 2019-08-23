89c93c6629084ca928f68397f13b46c9bc6c49ae
chenhaoxiang <uifuture@uifuture.com>
2019-08-23 20:04:34 +0800

    feature:编程式事务的处理

--- ssm-mybatis-transaction/src/test/java/com/uifuture/chapter17/BaseTest.java
+++ ssm-mybatis-transaction/src/test/java/com/uifuture/chapter17/BaseTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 /**
+ * 测试基类
  * @author chenhx
  * @version BaseTest.java, v 0.1 2019-08-23 00:53 chenhx
  */
