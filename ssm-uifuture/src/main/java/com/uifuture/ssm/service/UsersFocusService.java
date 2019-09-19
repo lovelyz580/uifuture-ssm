5cba304c0a1685f7f3190596c547574bbc354def
chenhaoxiang <uifuture@uifuture.com>
2019-09-20 00:10:41 +0800

    feature: 用户关注功能

---
+++ ssm-uifuture/src/main/java/com/uifuture/ssm/service/UsersFocusService.java
@@ -0,0 +1,16 @@
+package com.uifuture.ssm.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.uifuture.ssm.entity.UsersFocusEntity;
+
+/**
+ * <p>
+ * 用户关注表 服务类
+ * </p>
+ *
+ * @author chenhx
+ * @since 2019-09-19
+ */
+public interface UsersFocusService extends IService<UsersFocusEntity> {
+
+}
---
+++ ssm-uifuture/src/main/java/com/uifuture/ssm/service/impl/UsersFocusServiceImpl.java
@@ -0,0 +1,20 @@
+package com.uifuture.ssm.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.uifuture.ssm.entity.UsersFocusEntity;
+import com.uifuture.ssm.mapper.UsersFocusMapper;
+import com.uifuture.ssm.service.UsersFocusService;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 用户关注表 服务实现类
+ * </p>
+ *
+ * @author chenhx
+ * @since 2019-09-19
+ */
+@Service
+public class UsersFocusServiceImpl extends ServiceImpl<UsersFocusMapper, UsersFocusEntity> implements UsersFocusService {
+
+}
